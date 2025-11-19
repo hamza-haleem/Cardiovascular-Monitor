06bd8cd49bc723790b114e609873f0330782e5f4
Hazma Haleem <hamza.gcuf.edu@gmail.com>
2025-11-19 19:25:46 +0500

    Update cardiovascular_monitor.py

--- cardiovascular_monitor.py
+++ cardiovascular_monitor.py
@@ -490,7 +490,7 @@ if page == "📋 Diagnostic Report":
 
                     fig.update_traces(
                         hoverlabel=dict(
-                            bgcolor="#1E3A8A",     
+                            bgcolor="#143aa6",     
                             font_color="#F4F4F4",   
                             font_size=14
                         )
@@ -768,6 +768,7 @@ st.markdown(
     "</p>",
     unsafe_allow_html=True
 )
+
 
 
 
